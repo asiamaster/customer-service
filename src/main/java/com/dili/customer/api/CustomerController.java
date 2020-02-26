0fed22b0eb27ee65a51e34b86d51aba3d961b45f
岳洪波 <yuehongbo@diligrp.com>
2020-02-26 15:09:52 +0800

    增加客户禁用、启用接口，增加查询正常生效客户的接口

--- src/main/java/com/dili/customer/api/CustomerController.java
+++ src/main/java/com/dili/customer/api/CustomerController.java
@@ -18,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 由MyBatis Generator工具自动生成
@@ -38,6 +39,35 @@ public class CustomerController {
     @RequestMapping(value="/listPage", method = {RequestMethod.POST})
     public PageOutput<List<Customer>> listPage(@RequestBody(required = false) CustomerQueryInput customer){
         return customerService.listForPage(customer);
+    }
+
+    /**
+     * 分页查询正常的客户数据集
+     * 用户未删除切状态为生效的
+     * @param customer
+     * @return
+     */
+    @RequestMapping(value="/listNormalPage", method = {RequestMethod.POST})
+    public PageOutput<List<Customer>> listNormalPage(@RequestBody(required = false) CustomerQueryInput customer){
+        if (Objects.isNull(customer.getState())) {
+            customer.setState(1);
+        }
+        customer.setIsDelete(0);
+        return customerService.listForPage(customer);
+    }
+
+    /**
+     * 更新用户状态
+     * @param customerId 客户ID
+     * @param state      状态值
+     * @return
+     */
+    @RequestMapping(value = "/updateState", method = {RequestMethod.POST})
+    public BaseOutput updateState(@RequestParam("customerId") Long customerId, @RequestParam("state") Integer state) {
+        Customer condition = new Customer();
+        condition.setId(customerId);
+        condition.setState(state);
+        return BaseOutput.success().setData(customerService.updateSelective(condition));
     }
 
     /**
