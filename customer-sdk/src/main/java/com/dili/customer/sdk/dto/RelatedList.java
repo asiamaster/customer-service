0c83b6be8ab4c26253e24a62f7daa7baea360ca9
shaofan <akhuting@hotmail.com>
2020-09-21 14:14:20 +0800

    * 完善客户关联

--- customer-sdk/src/main/java/com/dili/customer/sdk/dto/RelatedList.java
+++ customer-sdk/src/main/java/com/dili/customer/sdk/dto/RelatedList.java
@@ -29,6 +29,6 @@ public class RelatedList extends BaseDomain {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @JSONField(format = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifyTime;
-    private Integer accountType;
+    private String customerMarketType;
 
 }
