ba9d9af534a43f90da628553e66514f6ae6cbd6f
岳洪波 <yuehongbo@diligrp.com>
2020-10-20 16:09:02 +0800

    根据手机号查询客户信息并按归属组织分组返回值修改

--- customer/src/main/java/com/dili/customer/domain/dto/CustomerMarketDto.java
+++ customer/src/main/java/com/dili/customer/domain/dto/CustomerMarketDto.java
@@ -4,6 +4,8 @@ import com.dili.customer.domain.CustomerMarket;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+
 /**
  * @author yuehongbo
  * @Copyright 本软件源代码版权归农丰时代科技有限公司及其研发团队所有, 未经许可不得任意复制与传播.
@@ -28,4 +30,14 @@ public class CustomerMarketDto extends CustomerMarket {
      * {@link com.dili.commons.glossary.YesOrNoEnum}
      */
     private Integer isCellphoneValid;
+
+    /**
+     * 证件号
+     */
+    private String certificateNumber;
+
+    /**
+     * 组织类型,个人/企业
+     */
+    private String organizationType;
 }
