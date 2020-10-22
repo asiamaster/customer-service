ee5338296f7ba7b688198e3a4aff669276f32038
岳洪波 <yuehongbo@diligrp.com>
2020-10-22 14:13:19 +0800

    增加本次sql调整

--- doc/change/20201017.sql
+++ doc/change/20201017.sql
@@ -1,1 +1,5 @@
 update customer set is_cellphone_valid = 0 where is_cellphone_valid is null;
+ALTER TABLE related_log
+MODIFY COLUMN `customer_name` varchar(40)  DEFAULT NULL COMMENT '客户名称' AFTER `id`;
+ALTER TABLE related_log
+MODIFY COLUMN `cardNo` varchar(200)  DEFAULT NULL COMMENT '园区卡号' AFTER `phone`;
