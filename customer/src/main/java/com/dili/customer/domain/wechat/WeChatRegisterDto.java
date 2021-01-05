83c7f7f173224e3e6d84692a7534c29809803f58
岳洪波 <yuehongbo@diligrp.com>
2021-01-05 15:42:47 +0800

    客户注册，增加手机要格式验证

--- customer/src/main/java/com/dili/customer/domain/wechat/WeChatRegisterDto.java
+++ customer/src/main/java/com/dili/customer/domain/wechat/WeChatRegisterDto.java
@@ -3,6 +3,7 @@ package com.dili.customer.domain.wechat;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 /**
  * 微信注册实体类型
@@ -22,6 +23,7 @@ public class WeChatRegisterDto {
      * 手机号
      */
     @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^(1[3456789]\\d{9})$", message = "请输入正确的手机号")
     private String cellphone;
 
     /**
