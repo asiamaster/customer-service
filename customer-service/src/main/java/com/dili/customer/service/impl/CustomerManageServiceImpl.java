35cade6b2fc4e65d40c1b482906899d54124e2f3
hanxi <1qaz2wsx>
2021-05-28 11:28:41 +0800

    从此分支剥离需求“当手机号对应客户已存在时，提示信息增加姓名和身份证号码”

--- customer-service/src/main/java/com/dili/customer/service/impl/CustomerManageServiceImpl.java
+++ customer-service/src/main/java/com/dili/customer/service/impl/CustomerManageServiceImpl.java
@@ -327,8 +327,7 @@ public class CustomerManageServiceImpl extends BaseServiceImpl<Customer, Long> i
         //如果为个人用户
         if (CustomerEnum.OrganizationType.INDIVIDUAL.equals(CustomerEnum.OrganizationType.getInstance(baseInfo.getOrganizationType()))) {
             if (CollectionUtil.isNotEmpty(phoneExist)) {
-                Customer phoneExistCustomer = phoneExist.get(0);
-                return "此手机号对应的客户已存在 (" + phoneExistCustomer.getName() + "： " + phoneExistCustomer.getCertificateNumber() + ")";
+                return "此手机号对应的客户已存在";
 
             }
         } else {
@@ -848,8 +847,7 @@ public class CustomerManageServiceImpl extends BaseServiceImpl<Customer, Long> i
             //如果为个人用户
             if (CustomerEnum.OrganizationType.INDIVIDUAL.equals(CustomerEnum.OrganizationType.getInstance(updateInput.getOrganizationType()))) {
                 if (CollectionUtil.isNotEmpty(phoneExist)) {
-                    Customer phoneExistCustomer = phoneExist.get(0);
-                    return BaseOutput.failure("此手机号对应的客户已存在 (" + phoneExistCustomer.getName() + "： " + phoneExistCustomer.getCertificateNumber() + ")");
+                    return BaseOutput.failure("此手机号对应的客户已存在";
                 }
             } else {
                 if (CollectionUtil.isNotEmpty(phoneExist) && phoneExist.size() >= customerConfig.getPhoneLimit()) {
